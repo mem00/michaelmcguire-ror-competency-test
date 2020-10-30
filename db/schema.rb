68d298713751336284fdd4b2b476e1d70d327f62
Michael McGuire <michael@mcguire.ch>
2020-10-30 15:56:42 -0400

    update schema to include user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_181423) do
+ActiveRecord::Schema.define(version: 2020_10_30_190037) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -19,4 +30,12 @@ ActiveRecord::Schema.define(version: 2020_10_30_181423) do
     t.index ["name"], name: "index_categories_on_name"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "articles", "categories"
+  add_foreign_key "articles", "users"
 end
