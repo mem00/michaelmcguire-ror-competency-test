e1e07a1f04dce24bdb87c0aa2e58122b439e8525
Michael McGuire <michael@mcguire.ch>
2020-10-30 21:27:01 -0400

    add show to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,12 @@
 class CategoriesController < ApplicationController
-
+ 
   def index
     @categories = Category.includes(:articles).all
+  end
+
+  def show
+    @category = Category.includes(:articles).find(params[:id])
+    @articles = @category.articles
   end
 
 
