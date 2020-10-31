d0239977b44a1faef96b2bd96dd7d0662014afbb
Michael McGuire <michael@mcguire.ch>
2020-10-30 21:27:18 -0400

    set up some forbidden methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def check_admin
     forbidden! unless logged_in?(:admin)
   end
+
+  def belongs_to_user_or_is_admin article
+    forbidden! unless logged_in?(:admin) || article.user == current_user
+  end
+
+  def check_user
+    forbidden! unless current_user
+  end
 end
